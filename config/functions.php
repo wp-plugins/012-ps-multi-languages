780ab3d4817567b157c54d8fa819a1eee905529a
ouhinit <ouhinit@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-15 05:34:14 +0000

    add fuction
    git-svn-id: https://plugins.svn.wordpress.org/012-ps-multi-languages/trunk@558333 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- config/functions.php
+++ config/functions.php
@@ -1,14 +1,25 @@
 <?php
 /*
  * Description: Custom post type funxtions
+ * Referring to the 「Prime Strategy Bread Crumb」
  * Author: Wangbin
 */
 	
-
-	function get_multi_lang_snippet( $lang = null , $echo = false ){
+	/**
+	* ファンクション名：ps_012_multilingual_list
+	* 機能概要：言語一覧を取得する
+	* 作成：プライム・ストラテジー株式会社 王 濱
+	* 作成：
+	* 変更：
+	* @param string $echo
+	* @param string $lang
+	* @param string $html/$multilingual_snippet
+	* @return
+	*/
+	function ps_012_multilingual_list( $echo = false, $lang = null ){
 		global $ps_multi_languages;
-		
-	 	$multilingual_snippet = $ps_multi_languages->get_multilingual_snippet( $lang );
+
+	 	$multilingual_snippet = $ps_multi_languages->ps_012_multilingual_list( $lang );
 		if ( !$echo ):
 			return $multilingual_snippet;
 		endif;
@@ -25,5 +36,55 @@
 		
 		echo $html;
 	}
+	
+	/**
+	* ファンクション名：get_load_language
+	* 機能概要：ロード言語を取得する
+	* 作成：プライム・ストラテジー株式会社 王 濱
+	* 作成：
+	* 変更：
+	* @param void
+	* @return $lang
+	*/	
+	function get_load_language( ){
+		global $ps_multi_languages;
+		
+		if ( $ps_multi_languages ){
+			$lang = $ps_multi_languages->get_load_lang( );
+		}
+		
+		return 	$lang;	
+	}
+	
+	/**
+	* ファンクション名：url_exists
+	* 機能概要：URLのありなし確認
+	* 作成：プライム・ストラテジー株式会社 王 濱
+	* 変更：
+	* @param string $url
+	* @return Boolean true/false
+	*/	
+	function url_exists($url) {
+	    if (!$fp = curl_init($url)) return false;
+	    return true;
+	}
+	
+	/**
+	* ファンクション名：ps_012_m17n_bread_crumb
+	* 機能概要：パンくずナビの取得
+	* 作成：プライム・ストラテジー株式会社 王 濱
+	* 作成：
+	* 変更：
+	* @param String $type
+	* @param Boolean $echo
+	* @param string $bread_crumb
+	* @return
+	*/
+	function ps_012_m17n_bread_crumb( $type = 'list', $echo = false ) {
+		global $ps_multi_languages;
+		$bread_crumb = $ps_multi_languages->bread_crumb( $type ,  $echo );
+		return $bread_crumb;
+	}
 
+	
 ?>
